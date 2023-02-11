7fa8c847d0223521c7de6878cc1cabc63a643942
Avanire <drtony@yandex.ru>
2023-02-11 14:48:06 +0600

    fix cart and design

--- resources/js/Components/cart-popup/cart-popup.tsx
+++ resources/js/Components/cart-popup/cart-popup.tsx
@@ -123,7 +123,7 @@ const CartPopup: FC = () => {
                                             </div>
                                             <div className="mt-6">
                                                 <Link
-                                                    href="cart"
+                                                    href="/cart"
                                                     onClick={() => modelCart.toggleCart(false)}
                                                     className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                                                 >
